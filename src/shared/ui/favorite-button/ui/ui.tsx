546db827b199122cb56ac2051990f7068668f692
Safirenat <saunakva@yandex.ru>
2026-03-11 21:31:06 +0300

    refactor: update favorite button styles

--- src/shared/ui/favorite-button/ui/ui.tsx
+++ src/shared/ui/favorite-button/ui/ui.tsx
@@ -2,20 +2,24 @@ import styles from './ui.module.scss'
 import { ButtonHTMLAttributes } from 'react'
 import clsx from 'clsx'
 import { useTranslations } from 'next-intl'
-import FavoriteIcon from '@/shared/assets/icons/heart.svg'
-
+import FavoriteIcon from '@/shared/assets/icons/favorite.svg'
 interface FavoriteButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string
+  pressed?: boolean
 }
 
-export function FavoriteButton({ className, ...props }: FavoriteButtonProps) {
+export function FavoriteButton({
+  className,
+  pressed = false,
+  ...props
+}: FavoriteButtonProps) {
   const t = useTranslations('Book')
 
   return (
     <button
       type='button'
       className={clsx(styles.button, className)}
-      aria-pressed='false'
+      aria-pressed={pressed}
       aria-label={t('buttonFavoriteAriaLabel')}
       {...props}
     >
